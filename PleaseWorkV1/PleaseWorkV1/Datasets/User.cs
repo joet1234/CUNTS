cfd9431bf27c607a13923a12605513505a68a830
Joseph Turkington <40082788@ads.qub.ac.uk>
2017-07-09 13:15:30 +0100

    TestV1

---
+++ PleaseWorkV1/PleaseWorkV1/Datasets/User.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace PleaseWorkV1.DataSets
+{
+    public class User
+    {
+        public String StudentNumber { set; get; }
+        public String Password { set; get; }
+
+        public User()
+        {
+
+        }
+
+        public User(String StudentNumber, String Password)
+        {
+            this.StudentNumber = StudentNumber;
+            this.Password = Password;
+        }
+
+        public bool ValidateUsername(String StudentNumber)
+        {
+            //Ensure 8 Letters 
+            if (StudentNumber.Length == 8)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        public bool withinDatabase()
+        {
+            return true;
+        }
+
+
+    }
+
+
+}
---
+++ PleaseWorkV1/PleaseWorkV1/Login.cs
@@ -0,0 +1,98 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace PleaseWorkV1
+{
+    [Activity(Label = "PleaseWorkV1", MainLauncher = true, Icon = "@drawable/icon")]
+    public class Login : Activity
+    {
+        public DataSets.User userOne = new DataSets.User();
+        
+        public EditText username;
+        public EditText password;
+        public Button loginButton;
+        public TextView forgotPass;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Login);
+
+            //Link with axml 
+            username = FindViewById<EditText>(Resource.Id.UsernameET);
+            password = FindViewById<EditText>(Resource.Id.PasswordET);
+            loginButton = FindViewById<Button>(Resource.Id.MyButton);
+            forgotPass = FindViewById<TextView>(Resource.Id.ForgotPassword);
+
+            // set onclick listener here, by deleting some process
+            loginButton.Click += delegate {
+                logonButtonClick();
+
+            };
+
+            forgotPass.Click += delegate
+            {
+                forgotPassClick();
+            };
+
+        }
+
+        public void logonButtonClick()
+        {
+            userOne.StudentNumber = username.Text;
+            userOne.Password = password.Text;
+
+            if (ValidateDetails() == true)
+            {
+                Toast.MakeText(this, "Move To Next", ToastLength.Long).Show();
+
+                StartActivity(typeof(Menu));
+
+
+            }
+        }
+
+        private void forgotPassClick()
+        {
+            Toast.MakeText(this, "Your Fucked", ToastLength.Long).Show();
+        }
+
+        public bool ValidateDetails()
+        {
+            //Username Correct
+            if (userOne.ValidateUsername(username.Text) == true)
+            {
+
+                if (userOne.Password.Length != 0)
+                {
+                    if (userOne.withinDatabase() == true)
+                    {
+                        // Check within Database
+                        Toast.MakeText(this, "Details Correct", ToastLength.Long).Show();
+                        return true;
+                    }
+                    else
+                    {
+                        Toast.MakeText(this, "Details Incorrect", ToastLength.Long).Show();
+                        return false;
+                    }
+                }
+                else
+                {
+                    Toast.MakeText(this, "Password Field Empty", ToastLength.Long).Show();
+                    return false;
+                }
+            }
+
+            else
+            {
+                Toast.MakeText(this, "Username Incorrect Format", ToastLength.Long).Show();
+                return false;
+            }
+        }
+    }
+}
+
+
